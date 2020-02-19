7dc256549fae95462ea77df0199e5cd0ee215226
George Karagkiaouris <gkaragkiaouris2@gmail.com>
2020-02-19 10:00:32 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,99 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
+const stream = require('stream');
+const {promisify} = require('util');
+const desktopIcons = require('hide-desktop-icons');
+const urlRegex = require('url-regex');
+const got = require('got');
+const tempfile = require('tempfile');
+
+const pipeline = promisify(stream.pipeline);
+
+const regex = urlRegex({exact: true});
+
+const wallpapers = {
+	Cheetah: 'http://512pixels.net/downloads/macos-wallpapers/10-0_10.1.png',
+	Puma: 'http://512pixels.net/downloads/macos-wallpapers/10-0_10.1.png',
+	Jaguar: 'http://512pixels.net/downloads/macos-wallpapers/10-2.png',
+	Panther: 'http://512pixels.net/downloads/macos-wallpapers/10-3.png',
+	Tiger: 'http://512pixels.net/downloads/macos-wallpapers/10-4.png',
+	Leopard: 'http://512pixels.net/downloads/macos-wallpapers/10-5.png',
+	'Snow Leopard': 'http://512pixels.net/downloads/macos-wallpapers/10-6.png',
+	Lion: 'http://512pixels.net/downloads/macos-wallpapers/10-7.png',
+	'Mountain Lion': 'http://512pixels.net/downloads/macos-wallpapers/10-8.jpg',
+	Mavericks: 'http://512pixels.net/downloads/macos-wallpapers/10-9.jpg',
+	Yosemite: 'http://512pixels.net/downloads/macos-wallpapers/10-10.jpg',
+	'El-Capitan': 'http://512pixels.net/downloads/macos-wallpapers/10-11.jpg',
+	Sierra: 'http://512pixels.net/downloads/macos-wallpapers/10-12.jpg',
+	'High Sierra': 'https://512pixels.net/downloads/macos-wallpapers/10-13.jpg'
+};
+
+const config = {
+	wallpaper: {
+		title: 'Wallpaper',
+		description: 'You can use your current wallpaper, one of the past macOS default wallpapers, or your own.',
+		enum: [
+			'Current wallpaper',
+			...Object.keys(wallpapers),
+			'Custom file or URL'
+		],
+		required: true,
+		default: 'Current wallpaper'
+	},
+	url: {
+		title: 'File or URL',
+		description: 'Will only be used if the option above is set to custom file or URL.',
+		type: 'string',
+		default: '',
+		// eslint-disable-next-line no-useless-escape
+		pattern: new RegExp(`(${regex.source})|(^\/([A-z0-9-_+]+\/)*([A-z0-9]+\.([A-z0-9-_+]+))$)`, 'i').source
+	}
+};
+
+const fetchImage = async url => {
+	try {
+		const file = tempfile(path.extname(url));
+
+		await pipeline(
+			got.stream(url),
+			fs.createWriteStream(file)
+		);
+
+		return file;
+	} catch {
+
+	}
+};
+
+const willStartRecording = async ({config}) => {
+	const wallpaper = config.get('wallpaper');
+
+	let filePath;
+	if (wallpaper !== 'Current wallpaper') {
+		if (wallpaper === 'Custom file or URL') {
+			const url = config.get('url');
+
+			if (regex.test(url)) {
+				filePath = await fetchImage(url);
+			} else if (fs.existsSync(url)) {
+				filePath = url;
+			}
+		}
+	}
+
+	return desktopIcons.hide(filePath);
+};
+
+const didStopRecording = async () => {
+	return desktopIcons.show();
+};
+
+const hideDesktopIcons = {
+	title: 'Hide desktop icons',
+	config,
+	willStartRecording,
+	didStopRecording
+};
+
+exports.shareServices = [hideDesktopIcons];
---
+++ readme.md
@@ -0,0 +1,13 @@
+# kap-hide-desktop-icons [![Build Status](https://travis-ci.org/karaggeorge/kap-hide-desktop-icons.svg?branch=master)](https://travis-ci.org/karaggeorge/kap-hide-desktop-icons)
+
+> [Kap](https://github.com/wulkano/kap) plugin - Hide your desktop icons while recording
+
+
+## Install
+
+In the `Kap` menu, go to `Preferences…`, select the `Plugins` pane, find this plugin, and toggle it.
+
+
+## Usage
+
+In the cropper or by right clicking the tray icon, click the `…` icon, then `Plugins` and make sure `Hide desktop icons` is enabled.
